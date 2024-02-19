c8840cb8feeea27058dc6cf3e1a3bfc66e2b2660
Julio Figueroa <jf338@usf.edu>
2024-02-19 11:16:32 -0500

    Inheritance

---
+++ Inheritance_EX.py
@@ -0,0 +1,19 @@
+# Base class for vehicles
+class Vehicle:
+    def __init__(self, brand, model):
+        self.brand = brand  # Initialize the brand attribute
+        self.model = model  # Initialize the model attribute
+
+    def drive(self):  # Define the drive method
+        print("The vehicle is moving forward.")
+
+    def stop(self):  # Define the stop method
+        print("The vehicle has stopped.")
+
+# Motorcycle class inherits from Vehicle
+class Motorcycle(Vehicle):
+    pass  # No additional attributes or methods, just inherits from Vehicle
+
+# Creating an instance of Motorcycle
+bike = Motorcycle('Harley-Davidson', 'Street 750')
+bike.drive()  # Calling the drive method from the Vehicle class
